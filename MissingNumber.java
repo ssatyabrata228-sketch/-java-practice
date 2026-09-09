11bff225326b6561b66bce20162a00b3739a72cc
ssatyabrata228-sketch <ssatyabrata228@gmail.com>
2026-09-09 12:02:20 +0530

    Missing Number Program

---
+++ MissingNumber.java
@@ -0,0 +1,17 @@
+public class MissingNumber {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 5};
+
+        int n = 5;
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+
+        int total = n * (n + 1) / 2;
+
+        System.out.println("Missing number = " + (total - sum));
+    }
+}
