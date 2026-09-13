28a3c5a83c0560ffd425a276efa41eb7510f88f3
Satyabrata Sahoo <ssatyabrata228@gmail.com>
2026-09-13 11:40:25 +0530

    Add linear search array problem

---
+++ Arrays/01_LinearSearch.java
@@ -0,0 +1,16 @@
+public class LinearSearch {
+    public static int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 30;
+        System.out.println("Index: " + search(arr, target));
+    }
+}
+
+// Time: O(n), Space: O(1)
